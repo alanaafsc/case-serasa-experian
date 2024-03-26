f9ced0a08461fde4d442e1aa3f2b247d1e7c311a
Alana Fernandes <48449273+alanaafsc@users.noreply.github.com>
2024-03-25 22:15:45 -0300

    feat: Adiciona componentes ao App.tsx

--- projeto-blog-pessoal/src/App.tsx
+++ projeto-blog-pessoal/src/App.tsx
@@ -1,26 +1,18 @@
+// App.tsx
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import HeaderNews from './components/HeaderNews/HeaderNews';
+import Artigos from './components/Artigos/Artigos';
+import FiltrarPorAutor from './components/FiltrarPorAutor/FiltrarPorAutor';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HeaderNews />
+      <FiltrarPorAutor />      
+      <Artigos />
     </div>
   );
-}
+};
 
 export default App;
+
